c93caaa02f64da009320f73279b88070d9a70fdd
kPhilosopher <agreee45@hotmail.com>
2013-05-12 17:29:23 -0600

    Having to deal with powershell regex for when the box_url is a number, where it tries to resolve it as an argument number

--- change_Vagrantfile_box_hostname.ps1
+++ change_Vagrantfile_box_hostname.ps1
@@ -1,2 +1,3 @@
 param($box_url)
-(Get-Content Vagrantfile) | Foreach-Object {$_ -replace "(http\:\/\/)([^/]+)(/)","`$1$box_url`$3"} | Set-Content Vagrantfile
+(Get-Content Vagrantfile) | Foreach-Object {$_ -replace "(http\:\/\/)([^/]+)(/)","`$1 $box_url`$3"} | Set-Content Vagrantfile
+(Get-Content Vagrantfile) | Foreach-Object {$_ -replace "(http\:\/\/) ([^/]+)(/)","`$1`$2`$3"} | Set-Content Vagrantfile
